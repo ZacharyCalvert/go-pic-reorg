445249ae9090e3840bdefaca73360ff6971ebc10
Zach Calvert <zachcalvert@gmail.com>
2022-09-13 10:12:51 -0500

    Kicking off the work again

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Somewhere around 10 years ago I built a media reviewer, tagging and organization service.  This service hasn't really been maintained, and the storage method made it impossible to organize by date, which in hindsight was really silly.
 
 The database is a simple raw YAML format.  This simple golang library is to convert that content into a flat folder structure, making it much easier to navigate and review. 
+
+While open source, this serves no purpose outside of my legacy media manager.  
--- main.go
+++ main.go
@@ -9,40 +9,18 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// **** Goals Below ****
-// commands:
-// - stats
-// - reorg
-// - add new folder
-// - web
-// jpeg metadata utility
-
-var commands = [...]string{"stats", "reorg", "add", "web"}
-
 func main() {
 
 	managedHelp := "Path to managed media directory"
-	comandHelp := commandOptionsToString() + ": Statistics, reorganize, add folder, and web API start."
-	var managed, command string
+	targetHelp := "Path to target reorg directory"
+	var managed, target string
 	flag.StringVar(&managed, "managed", ".", managedHelp)
 	flag.StringVar(&managed, "m", ".", managedHelp+" (shorthand)")
-	flag.StringVar(&command, "command", "stats", comandHelp)
-	flag.StringVar(&command, "c", "stats", comandHelp+" (shorthand)")
+	flag.StringVar(&target, "target", "", targetHelp)
+	flag.StringVar(&target, "t", "", targetHelp+" (shorthand)")
 	flag.Parse()
 	records := loadRecordDatabase(managed)
 	fmt.Printf("Record count: %d", len(records))
-}
-
-func commandOptionsToString() string {
-	result := ""
-	for i, v := range commands {
-		if i == 0 {
-			result = v
-		} else {
-			result = fmt.Sprintf("%s|%s", result, v)
-		}
-	}
-	return result
 }
 
 func loadRecordDatabase(path string) map[string]db.MediaRecord {
